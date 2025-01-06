629bc1d23f875cb8bd6b60148af3cb5502210280
Gabriel Coelho <153249032+gabrielcoelhosev@users.noreply.github.com>
2025-01-06 19:12:24 -0300

    feat: Página de Favoritos

--- src/pages/Favorito/index.js
+++ src/pages/Favorito/index.js
@@ -1,7 +1,52 @@
+import { useEffect, useState } from 'react';
+import './favoritos.css';
+import { Link } from 'react-router-dom';
+
 function Favorito(){
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+
+        const minhaLista = localStorage.getItem('@primeflix');
+        setFilmes(JSON.parse(minhaLista) || []);
+
+
+    }, []);
+
+    function excluirFilme(id){
+        let filtroFilmes = filmes.filter((item) => {
+            //devolve todos os itens que são diferentes do id que foi passado
+            return(item.id !== id)
+        })
+
+        alert('Filme excluído com sucesso!');
+
+        setFilmes(filtroFilmes);
+
+        //Atualiza a lista de filmes, sem o filme que foi excluído.
+        localStorage.setItem('@primeflix', JSON.stringify(filtroFilmes));
+    }
+
     return(
-        <div>
-            <h1>Favoritos</h1>
+        <div className='meus-filmes'>
+            <h1>Meus Filmes</h1>
+
+            {filmes.length === 0 && <span>Você não possui nenhum filme salvo :(</span>}
+            <ul>
+                {filmes.map((item) => {
+                    return(
+                        <li key={item.id}>
+                            <span>{item.title}</span>
+                            <div>
+                                <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
+                                <button onClick={() => excluirFilme(item.id)}>
+                                    Excluir
+                                </button>
+                            </div>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     );
 }
--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,10 +1,12 @@
 import {useEffect, useState} from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import './filme-info.css';
 
 import api from '../../services/api';
 
 function Filme(){
     const {id} = useParams();
+    const navigate = useNavigate();
     const [filme, setFilme] = useState({});
     const [loading, setLoading] = useState(true);
 
@@ -21,7 +23,7 @@ function Filme(){
                 setLoading(false);
             })
             .catch(() => {
-            
+                navigate('/', { replace: true});
             })
         }
 
@@ -30,7 +32,25 @@ function Filme(){
         return () => {
             console.log('Componente desmontado');
         }
-    }, [])
+    }, [navigate, id]);
+
+    function salvarFilme(){
+        const minhaLista = localStorage.getItem("@primeflix");
+
+        let filmesSalvos = JSON.parse(minhaLista) || [];
+    
+
+        const hasFilme = filmesSalvos.some((filmeSalvo) => filmeSalvo.id === filme.id);
+
+        if(hasFilme){
+            alert('Você já possui esse filme salvo');
+            return;
+        }
+
+        filmesSalvos.push(filme);
+        localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+        alert("Filme salvo com sucesso!"); 
+    }
 
     if(loading){
         return(
@@ -42,7 +62,19 @@ function Filme(){
 
     return(
         <div className='filme-info'>
+            <h1>{filme.title}</h1>
             <img src={`https://image.tmdb.org/t/p/original${filme.poster_path}`} alt={filme.title} />
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+
+            <strong>Avaliação: {filme.vote_average} / 10 </strong>
+
+            <div className='area-buttons'>
+                <button onClick={salvarFilme}>Salvar</button>
+                <button><a target='black' rel='external' href={`https://youtube.com/results?search_querry=${filme.title} Trailer`}>Trailer</a></button>
+            </div>
+
         </div>
     );
 }
